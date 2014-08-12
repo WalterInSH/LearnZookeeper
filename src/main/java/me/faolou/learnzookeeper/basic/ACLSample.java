32b6cddba26a60775598abdf4aa0500af2d4e928
Walter <coolwaterever@gmail.com>
2014-08-12 16:14:29 +0800

    add acl provider

--- src/main/java/me/faolou/learnzookeeper/basic/ACLSample.java
+++ src/main/java/me/faolou/learnzookeeper/basic/ACLSample.java
@@ -10,7 +10,7 @@ import org.apache.zookeeper.server.ServerCnxn;
 import org.apache.zookeeper.server.auth.AuthenticationProvider;
 
 /**
- * Access Control List
+ * Access Control List (tbd)
  * Created by zhangkai on 8/10/14.
  */
 public class ACLSample {
@@ -30,42 +30,44 @@ public class ACLSample {
         };
         ZooKeeper zk = new ZooKeeper("127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183", 3000, watcher);
 
-
+        zk.close();
     }
+
+    class NameStartsWithAAuthenticationProvider implements AuthenticationProvider{
+
+        @Override
+        public String getScheme() {
+            return "nswa";
+        }
+
+        @Override
+        public KeeperException.Code handleAuthentication(ServerCnxn cnxn, byte[] authData) {
+            String name = new String(authData);
+            if (StringUtils.isBlank(name)) {
+                return KeeperException.Code.AUTHFAILED;
+            }
+            cnxn.getAuthInfo().add(new Id(getScheme(), name));
+            return KeeperException.Code.OK;
+        }
+
+        @Override
+        public boolean matches(String id, String aclExpr) {
+            if (StringUtils.isBlank(id) || StringUtils.isBlank(aclExpr)) {
+                return false;
+            }
+            return StringUtils.startsWith(id, "A");
+        }
+
+        @Override
+        public boolean isAuthenticated() {
+            return true;
+        }
+
+        @Override
+        public boolean isValid(String id) {
+            return StringUtils.isNotBlank(id) && id.length() > 1;
+        }
+    }
+
 }
 
-class NameStartsWithAAuthenticationProvider implements AuthenticationProvider{
-
-    @Override
-    public String getScheme() {
-        return "nswa";
-    }
-
-    @Override
-    public KeeperException.Code handleAuthentication(ServerCnxn cnxn, byte[] authData) {
-        String name = new String(authData);
-        if (StringUtils.isBlank(name)) {
-            return KeeperException.Code.AUTHFAILED;
-        }
-        cnxn.getAuthInfo().add(new Id(getScheme(), name));
-        return KeeperException.Code.OK;
-    }
-
-    @Override
-    public boolean matches(String id, String aclExpr) {
-        if (StringUtils.isBlank(id) || StringUtils.isBlank(aclExpr)) {
-            return false;
-        }
-        return StringUtils.startsWith(id, "A");
-    }
-
-    @Override
-    public boolean isAuthenticated() {
-        return true;
-    }
-
-    @Override
-    public boolean isValid(String id) {
-        return StringUtils.isNotBlank(id) && id.length() > 1;
-    }
-}
