ca83bf413feb209f6f0a9fdd8387740a725ac02a
Walter <coolwaterever@gmail.com>
2014-08-20 19:10:14 +0800

    add curator crud

---
+++ src/main/java/me/faolou/learnzookeeper/curator/CuratorCRUDSample.java
@@ -0,0 +1,43 @@
+package me.faolou.learnzookeeper.curator;
+
+import org.apache.curator.RetryPolicy;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.apache.zookeeper.data.Stat;
+
+import java.util.List;
+
+/**
+ * @author Walter Zhang
+ */
+public class CuratorCRUDSample {
+
+    public static void main(String[] args) throws Exception {
+        RetryPolicy retryPolicy = new ExponentialBackoffRetry(1000, 3);
+        CuratorFramework client = CuratorFrameworkFactory.newClient("127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183", retryPolicy);
+        client.start();
+
+        //创建节点
+        client.create().forPath("/CuratorNode", "test curator".getBytes());
+        //判断是否创建成功(存在性)
+        Stat stat = client.checkExists().forPath("/CuratorNode");
+        System.out.println("CuratorNode " + stat != null ? "exists" : "not exists");
+        System.out.println(new String(client.getData().forPath("/CuratorNode")));
+
+        //创建子节点
+        client.create().forPath("/CuratorNode/ChildNode","curator child".getBytes());
+
+        //获取子节点
+        List<String> children = client.getChildren().forPath("/CuratorNode");
+        System.out.println("Children are : " + children);
+
+        //修改子节点数据
+        client.setData().forPath("/CuratorNode/ChildNode", "curator little child".getBytes());
+        System.out.println(new String(client.getData().forPath("/CuratorNode/ChildNode")));
+
+        client.delete().forPath("/CuratorNode/ChildNode");
+        client.delete().forPath("/CuratorNode");
+        client.close();
+    }
+}
