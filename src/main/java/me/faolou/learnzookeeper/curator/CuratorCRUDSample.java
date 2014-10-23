2aca4233003b7e259609e11d7bdb69ece7fd1ba9
jack.zhang <jack.zhang@vipshop.com>
2014-10-23 19:26:57 +0800

    zk createIfParent

--- src/main/java/me/faolou/learnzookeeper/curator/CuratorCRUDSample.java
+++ src/main/java/me/faolou/learnzookeeper/curator/CuratorCRUDSample.java
@@ -25,8 +25,8 @@ public class CuratorCRUDSample {
         client.create().forPath("/CuratorNode", "test curator".getBytes());
         //判断是否创建成功(存在性)
         Stat stat = client.checkExists().forPath("/CuratorNode");
-        System.out.println("version:"+stat.getVersion());
-        System.out.println("Aversion:"+stat.getAversion());
+        System.out.println("version:" + stat.getVersion());
+        System.out.println("Aversion:" + stat.getAversion());
         System.out.println("Cversion:" + stat.getCversion());
         System.out.println("pxid:" + stat.getPzxid());
         System.out.println("cxid:" + stat.getCzxid());
@@ -42,23 +42,23 @@ public class CuratorCRUDSample {
         System.out.println(new String(client.getData().forPath("/CuratorNode")));
 
         //创建子节点
-        client.create().forPath("/CuratorNode/ChildNode","curator child".getBytes());
+        client.create().forPath("/CuratorNode/ChildNode", "curator child".getBytes());
 
         //获取子节点
         List<String> children = client.getChildren().forPath("/CuratorNode");
         System.out.println("Children are : " + children);
 
         //修改子节点数据
-        client.setData().forPath("/CuratorNode/ChildNode", "curator little child".getBytes());
-        System.out.println(new String(client.getData().forPath("/CuratorNode/ChildNode")));
+        client.create().creatingParentsIfNeeded().forPath("/CuratorNode/ChildNode/1/1", "curator little child".getBytes());
+        System.out.println("createParent:" + new String(client.getData().forPath("/CuratorNode/ChildNode/1/1")));
 
         //修改主节点数据
-        client.setData().forPath("/CuratorNode","fuck".getBytes());
+        client.setData().forPath("/CuratorNode", "fuck".getBytes());
 
         //判断是否创建成功(存在性)
         stat = client.checkExists().forPath("/CuratorNode");
-        System.out.println("version:"+stat.getVersion());
-        System.out.println("Aversion:"+stat.getAversion());
+        System.out.println("version:" + stat.getVersion());
+        System.out.println("Aversion:" + stat.getAversion());
         System.out.println("Cversion:" + stat.getCversion());
         System.out.println("pxid:" + stat.getPzxid());
         System.out.println("cxid:" + stat.getCzxid());
@@ -69,7 +69,7 @@ public class CuratorCRUDSample {
 
         byte[] a = new byte[1];
         DataInput input = new DataInputStream(new ByteArrayInputStream(a));
-stat.readFields(input);
+        stat.readFields(input);
         System.out.println(a);
 
         client.delete().forPath("/CuratorNode/ChildNode");
